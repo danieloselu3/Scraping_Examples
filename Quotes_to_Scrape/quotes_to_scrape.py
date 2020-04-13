efb776f3ab59587b567bb711f36153150f61333a
Daniel Oselu <doselu3@gmail.com>
2020-04-13 14:45:36 +0300

    added the next page functionality

--- Quotes_to_Scrape/quotes_to_scrape.py
+++ Quotes_to_Scrape/quotes_to_scrape.py
@@ -1,36 +1,3 @@
-# # import
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "http://quotes.toscrape.com/"
-
-# session = requests.Session()
-# headers = {'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-# 'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
-
-# html = session.get(url, headers=headers)
-# bsObj = BeautifulSoup(html.text, 'lxml')
-
-# # print(html.status_code)
-# # print(html.headers)
-
-# for target_cell in bsObj.select('div.quote'):
-#     author_quote = target_cell.find('span', {'class':'text'}).get_text()
-#     print(author_quote)
-#     print("------------------")
-#     author_name = target_cell.find('small', {'class':"author"}).get_text()
-#     print(author_name)
-
-# # data_from_target_cell_1 = []
-
-# # def getQuotes():
-# #     for target_cell in bsObj.select('div.quote'):
-# #         # author_quote = target_cell.find('span', {'class':"text"}).get_text()
-# #         author_name = target_cell.find('small', {'class':"author"}).get_text()
-# #         print(author_name)
-
-
-
 # imports
 import requests
 from bs4 import BeautifulSoup
@@ -41,18 +8,18 @@ session = requests.Session()
 headers = {'Accept':"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
 
-
-# url = "http://quotes.toscrape.com"
+# base url
+base_url = "http://quotes.toscrape.com"
 
 # Create temporary data holders
 Quotes_Holder = []
-Author_Link_Holder = []
+Author_Link_Holder = set()
 Author_Description_Holder = []
 
 # First Method (Extracts data from Target cell 1)
 def getQuotes(url):
     """ Scrapes data from A single 'Quote to scrape' page """
-    # try catchhing any error from the server
+    # try catching any error from the server
     try:
         html = session.get(url)
     except HTTPError as e:
@@ -64,7 +31,7 @@ def getQuotes(url):
     except AttributeError as e:
         return None
 
-    for target_cell in bsObj.select('div.quote')[:5]:
+    for target_cell in bsObj.select('div.quote'):
         quote = target_cell.find('span', {'class':'text'}).get_text()
         author_of_quote = target_cell.find('small', {'class':'author'}).get_text()
         quote_tags = target_cell.find('meta', {'class':'keywords'})['content']
@@ -76,22 +43,30 @@ def getQuotes(url):
             'Tags':quote_tags
         })
 
-        Author_Link_Holder.append(link_to_author_description)
+        Author_Link_Holder.add(link_to_author_description)
+    
+    # catcch any attribute error thrown by bs4 incase our target is empty
+    try:
+        next_url = bsObj.find('li', {'class':'next'}).find('a')['href']
+    except AttributeError as e:
+        return None
+
+    if next_url is not None:
+        next_page_to_scrape = base_url + next_url
+        getQuotes(next_page_to_scrape)
+    else:
+        print('no more pages to scrape')
 
 
 # getQuotes("http://quotes.toscrape.com")
 # print(len(Quotes_Holder))
-# for i in Quotes_Holder:
+# for i in Quotes_Holder[-1:]:
 #     print(i)
 #     print("-----")
-
+# print(len(Author_Link_Holder))
 # for i in Author_Link_Holder:
 #     print(i)
 #     print("-----")
 
-
-
-
-# Second Method (A method to get the next pages)
 
 
